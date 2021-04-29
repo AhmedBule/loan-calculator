041e4dcdc4491ebdb1cfda20e1c9f45ed1288754
Ahmed Bule <ahmedbule01@gmail.com>
2021-04-29 23:05:16 +0300

    added the formula for the rate

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-alert('Hi there')
+//alert('Testing to see if it works')
 
-const amount = document.getElementById('#amount');
+function loanCalculator() {
+
+
+
+    const amount = document.querySelector('.amount').value;
+    const monthsPay = document.querySelector('.months').value;
+    const interestRate = document.querySelector('.rate').value;
+
+    // The formula for the calculation 
+
+    let interest = (amount * (rate * 0.01)) / months;
+
+}
